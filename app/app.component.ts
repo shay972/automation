8db9afd6ac3bb3ac7d296484652a44cdcbec5a7a
shay972 <shay.binyat@gmail.com>
2016-09-20 16:40:16 +0300

    Add files via upload
    fixed garbitch

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sb-root',  
   templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css']
+  styleUrls: ['app/app.component.css'],
 })
 export class AppComponent { }
